6a4010c9897e7cddfaa293e9a0cf95a3f5e08eda
Vusaldidaaaaa <99710983+Vusaldidaaaaa@users.noreply.github.com>
2022-03-11 13:11:46 +0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-	token: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-	botId: 1234567890, //first numbers of BOT_TOKEN
+	token: "5271153957:AAFFxkRFp5XQD9spSItt0-hA0g1uO64gaLg",
+	botId: 5271153957, //first numbers of BOT_TOKEN
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 3, //timer indicators
